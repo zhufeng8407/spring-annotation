5b58e2448c25e8b95d3b62e52a15484cc60d2832
zhufeng <zhufeng@10.8.40.46>
2018-09-13 16:34:42 +0800

    2018/9/13 import study zhufeng

--- src/main/java/com/spring/config/MainConfig2.java
+++ src/main/java/com/spring/config/MainConfig2.java
@@ -1,14 +1,15 @@
 package com.spring.config;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Lazy;
 
+import com.spring.bean.Color;
 import com.spring.bean.Person;
 
 @Configuration
@@ -17,17 +18,22 @@ import com.spring.bean.Person;
 		@Filter(type = FilterType.ANNOTATION, classes = {Controller.class}),
 		@Filter(type = FilterType.CUSTOM, classes = {MyTypeFilter.class})
 }, useDefaultFilters = false)
-public class MainConfig {
+@Import(Color.class)	//Id默认是组件的全类名
+public class MainConfig2 {
 	
 	/**
 	 * Scope	注解See ConfigurableBeanFactory # SCOPE_PROTOTYPE
 	 * SCOPE_PROTOTYPE 多实例 启动时不会调用
 	 * SCOPE_SINGLETON 单例默认	启动时调用
-	 * @return
+	 * 
+	 * 懒加载:
+	 * 		单实例bean:默认在容器启动的时候创建对象
+	 * 		懒加载：容器启动时不创建对象，第一次使用时创建
 	 */
-	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	@Lazy
 	@Bean
 	public Person person() {
-		return new Person("lijian", 40);
+		return new Person("zhangsan", 40);
 	}
 }
