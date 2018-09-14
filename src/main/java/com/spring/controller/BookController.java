9fd3032521242b22a36821b9b1cbcf9fdae59811
zhufeng <zhufeng@10.8.40.46>
2018-09-14 14:35:48 +0800

    2018/9/13 IOCtest追加		zhufeng

--- src/main/java/com/spring/controller/BookController.java
+++ src/main/java/com/spring/controller/BookController.java
@@ -1,10 +1,23 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.spring.service.BookService;
 
 @Controller
 public class BookController {
 
+	@Autowired
+	private BookService bookService;
 	
+	public void print() {
+		System.out.println(bookService);
+	}
+
+	@Override
+	public String toString() {
+		return "BookController [bookService=" + bookService + "]";
+	}
 	
 }
